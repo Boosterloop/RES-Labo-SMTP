d721e5a73512e2252ceeb13ca52c7091b6ae858e
X-Brast <alx-brast@outlook.com>
2019-04-08 13:57:29 +0200

    class Person + ISmtpClient fini

--- src/model/mail/Person.java
+++ src/model/mail/Person.java
@@ -2,4 +2,35 @@ package model.mail;
 
 public class Person {
 
+	private String nom;
+	private String prenom;
+	private String email;
+	
+	public Person(String nom, String prenom, String email) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.email = email;
+	}
+
+	/**
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+
+	/**
+	 * @return the prenom
+	 */
+	public String getPrenom() {
+		return prenom;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	
 }
